0a75c095f9e5f4f6aeb7bfb88dbd1f164b98697a
EC2 Default User <ec2-user@ip-172-31-10-166.ap-northeast-1.compute.internal>
2022-09-18 01:45:25 +0000

    [Finish]4-25/Bootstrapを使うために必要な記述を追加する

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
+  resources :users only: [:show, :edit]
+  resources :books only: [:new, :index, :show, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
