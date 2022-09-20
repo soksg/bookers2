4ab1f1deda41783bbcfa4160b094353eee462b15
EC2 Default User <ec2-user@ip-172-31-10-166.ap-northeast-1.compute.internal>
2022-09-20 11:04:29 +0000

    [Start]4-25 user/index作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+  def index
+    @users=User.all
+  end
+
   def show
     @user=User.find(params[:id])
     @books=@user.books
@@ -10,7 +14,7 @@ class UsersController < ApplicationController
 
   def update
     @user=User.find(params[:id])
-    @user.update
+    @user.update(user_params)
     redirect_to user_path(@user.id)
   end
 
@@ -18,7 +22,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :profile_image)
+    params.require(:user).permit(:name)
   end
 
 end
