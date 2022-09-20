a460707480765e2e71aae2188f5b9b6bfe6c30c9
EC2 Default User <ec2-user@ip-172-31-10-166.ap-northeast-1.compute.internal>
2022-09-20 07:26:20 +0000

    [Fix]4-25/users_edit画面へのリンク作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :books, dependent: :destroy
+  
+  # def get_profile_image(width,height)
+  #   unless profile
 end
