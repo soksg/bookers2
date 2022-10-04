1423c50f799b3fcb53b96ef60230bc8a2011317e
EC2 Default User <ec2-user@ip-172-31-10-166.ap-northeast-1.compute.internal>
2022-10-04 08:09:02 +0000

    [Add]応用課題６ コメント機能非同期通信化

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -5,12 +5,14 @@ class BookCommentsController < ApplicationController
     comment=current_user.book_comments.new(book_comment_params)
     comment.book_id=book.id
     comment.save
-    redirect_to request.referer
+    # redirect_to request.referer
   end
 
   def destroy
+    # book=BookComment.find(params[:id])
+    # book.destroy
     BookComment.find(params[:id]).destroy
-    redirect_to request.referer
+    # redirect_to request.referer
   end
 
   private
